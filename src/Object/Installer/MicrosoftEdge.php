7a739771c30d94bac3b0f897e2ced8b15418f18f
Aaron J <am@jonesiscoding.com>
2021-12-29 14:56:05 -0600

    Adds configuration for Microsoft Edge installation.

---
+++ src/Object/Installer/MicrosoftEdge.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace DevCoding\Jss\Easy\Object\Installer;
+
+/**
+ * Installer configuration class for Microsoft Edge. Credit to MacAdmins.software for the link id, current version
+ * information, and package name.
+ *
+ * @author  AMJones <am@jonesiscoding.com>
+ * @license https://github.com/deviscoding/jss-helper/blob/main/LICENSE
+ */
+class MicrosoftEdge extends MicrosoftInstaller
+{
+  public function getName()
+  {
+    return 'Microsoft Edge';
+  }
+
+  public function getPath()
+  {
+    return '/Applications/Microsoft Edge.app';
+  }
+
+  protected function getLinkId()
+  {
+    return 2093504;
+  }
+
+  protected function getPackageName()
+  {
+    return 'com.microsoft.edge';
+  }
+
+  public function getCurrentVersion()
+  {
+    return $this->getVersionFromUrl($this->getDestinationUrl(), '#[a-zA-Z-]*(?<version>[0-9.]*)\.pkg#');
+  }
+}
